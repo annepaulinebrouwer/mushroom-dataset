81e2b0114f481dbcdf31024f14d7014fea6d4683
Paula Brouwer <freelance.brouwer@gmail.com>
2023-07-31 20:44:51 +0200

    Populate the database with the mushroom dataset

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'csv'
+
+filepath = "db/mushroom_dataset.csv"
+CSV.foreach(filepath, headers: :first_row) do |row|
+  Mushroom.create(
+    edible: row["edible"],
+    cap_shape: row["cap_shape"],
+    cap_surface: row["cap_surface"],
+    cap_color: row["cap_color"],
+    bruises: row["bruises"] == "BRUISES",
+    odor: row["odor"],
+    gill_attachment: row["gill_attachment"],
+    gill_spacing: row["gill_spacing"],
+    gill_size: row["gill_size"],
+    gill_color: row["gill_color"],
+    stalk_shape: row["stalk_shape"],
+    stalk_root: row["stalk_root"],
+    stalk_surface_above_ring: row["stalk_surface_above_ring"],
+    stalk_surface_below_ring: row["stalk_surface_below_ring"],
+    stalk_color_above_ring: row["stalk_color_above_ring"],
+    stalk_color_below_ring: row["stalk_color_below_ring"],
+    veil_type: row["veil_type"],
+    veil_color: row["veil_color"],
+    ring_number: row["ring_number"] == "ONE" ? 1 : 2,
+    ring_type: row["ring_type"],
+    spore_print_colororange: row["spore_print_colororange"],
+    population: row["population"],
+    habitat: row["habitat"]
+  )
+end
